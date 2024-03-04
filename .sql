b628a772c7344e6010fda892c35659a032474fbe
renantescaro <renantescaro@yahoo.com.br>
2024-03-04 19:29:31 -0300

    feature: create .sql with database

--- .sql
+++ .sql
@@ -1,7 +1,36 @@
 
 create database objective_test;
 
-create table account(
-	id int not null auto_increment,
-    primary key(id)
-);
+CREATE TABLE `fee` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `payment_method` enum('PIX','CREDIT_CARD','DEBIT_CARD') DEFAULT NULL,
+  `amount` float DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `ix_fee_id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `account` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `account_number` varchar(9) DEFAULT NULL,
+  `document` varchar(20) DEFAULT NULL,
+  `amount` float DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `account_number` (`account_number`),
+  UNIQUE KEY `document` (`document`),
+  KEY `ix_account_id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `transaction` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `amount` float DEFAULT NULL,
+  `payment_method` enum('PIX','CREDIT_CARD','DEBIT_CARD') DEFAULT NULL,
+  `transaction_kind` enum('INPUT','OUTPUT') DEFAULT NULL,
+  `account_id` int(11) DEFAULT NULL,
+  `timestamp` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `account_id` (`account_id`),
+  KEY `ix_transaction_id` (`id`),
+  CONSTRAINT `transaction_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `account` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
